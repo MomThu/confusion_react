b9afe960795374188fdf37f1b7866be6ffd33131
Mom Thu <thumomm10@gmail.com>
2021-06-12 21:53:04 +0700

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Link} from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = (val) => val && val.length;
@@ -94,7 +95,7 @@ class CommentForm extends Component {
     function RenderDish ({dish}) {
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
